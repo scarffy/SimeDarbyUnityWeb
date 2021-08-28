524d9703ad34a5f7701043f62ebb2fd0900ea507
scarffy <34879344+scarffy@users.noreply.github.com>
2021-08-28 12:36:56 +0800

    Added Elmina and Sime Darby Logo and OpenSection script

--- Assets/Script/OpenSection.cs
+++ Assets/Script/OpenSection.cs
@@ -9,15 +9,101 @@ public class OpenSection : MonoBehaviour
     public GameObject[] RightWall;
     public GameObject[] OtherBuildings;
 
-    // Start is called before the first frame update
-    void Start()
+    [Space(20)]
+    [SerializeField] bool RoofBool = false;
+    [SerializeField] bool LeftWallBool = false;
+    [SerializeField] bool RightWallBool = false;
+    [SerializeField] bool OtherBuildingsBool = false;
+
+    public void RoofLogic()
     {
-        
+        if(Roof.Length > 0)
+        {
+            if (RoofBool)
+            {
+                for (int i = 0; i < Roof.Length; i++)
+                {
+                    Roof[i].SetActive(true);
+                }
+                RoofBool = false;
+            }
+            else
+            {
+                for (int i = 0; i < Roof.Length; i++)
+                {
+                    Roof[i].SetActive(false);
+                }
+                RoofBool = true;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LeftWallLogic()
     {
-        
+        if (LeftWall.Length > 0)
+        {
+            if (LeftWallBool)
+            {
+                for (int i = 0; i < LeftWall.Length; i++)
+                {
+                    LeftWall[i].SetActive(true);
+                }
+                LeftWallBool = false;
+            }
+            else
+            {
+                for (int i = 0; i < LeftWall.Length; i++)
+                {
+                    LeftWall[i].SetActive(false);
+                }
+                LeftWallBool = true;
+            }
+        }
+    }
+
+    public void RightWallLogic()
+    {
+        if (RightWall.Length > 0)
+        {
+            if (RightWallBool)
+            {
+                for (int i = 0; i < RightWall.Length; i++)
+                {
+                    RightWall[i].SetActive(true);
+                }
+                RightWallBool = false;
+            }
+            else
+            {
+                for (int i = 0; i < RightWall.Length; i++)
+                {
+                    RightWall[i].SetActive(false);
+                }
+                RightWallBool = true;
+            }
+        }
+    }
+
+    public void OtherBuildingLogic()
+    {
+        if (OtherBuildings.Length > 0)
+        {
+            if (OtherBuildingsBool)
+            {
+                for (int i = 0; i < OtherBuildings.Length; i++)
+                {
+                    OtherBuildings[i].SetActive(true);
+                }
+                OtherBuildingsBool = false;
+            }
+            else
+            {
+                for (int i = 0; i < OtherBuildings.Length; i++)
+                {
+                    OtherBuildings[i].SetActive(false);
+                }
+                OtherBuildingsBool = true;
+            }
+        }
     }
 }
