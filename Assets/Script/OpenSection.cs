6d8f2c1b149e7778b260b31c13f787bde36a9dff
scarffy <34879344+scarffy@users.noreply.github.com>
2021-08-28 12:07:19 +0800

    Added open and close section script

---
+++ Assets/Script/OpenSection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSection : MonoBehaviour
+{
+    public GameObject[] Roof;
+    public GameObject[] LeftWall;
+    public GameObject[] RightWall;
+    public GameObject[] OtherBuildings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
